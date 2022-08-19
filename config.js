a499521ec633b4da845d79f6392a504da0ad6fc4
Bomiro <105790086+Bomiro@users.noreply.github.com>
2022-08-19 19:08:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url'
 global.owner = [
   ['6281390658325'],
   ['6282256115584'],
-  ['6281319944687', 'BG', true]
+  ['212774678871', 'SPY X', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -30,8 +30,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot'
+global.packname = 'wa.me/212705570169'
+global.author = 'Namx Bot'
 
 global.multiplier = 69 // The higher, The harder levelup
 
